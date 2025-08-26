5211057babb1eb7038c680708e7e8e5977ba790d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2025-08-26 13:11:34 +0600

    Server vs Client Components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,16 @@
+import ButtonComponent from "./button";
 
-export default function Contact(){
-    return (
-        <div>
-        Contact Page
-        </div>
-    )
+export default async function Contact() {
+  console.log("Contact Page");
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  console.log(posts);
+
+  return (
+    <div>
+
+     <ButtonComponent/>
+    </div>
+  );
 }
