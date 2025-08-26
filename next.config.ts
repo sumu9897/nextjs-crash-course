a12166e03d25f9ee57e318881bb6abb22e25091c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2025-08-26 08:38:22 +0600

    Working with images in next js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[
+     { hostname: 'upload.wikimedia.org'},
+    ]
+  }
 };
 
 export default nextConfig;
